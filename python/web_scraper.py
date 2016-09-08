af12742ddb96f4e6298d5599add6f6452260a3f3
cmking54 <cmking5496@gmail.com>
2016-09-07 22:35:47 -0400

    Separated the work; Gotten timetables, need to put into sqlite

--- python/web_scraper.py
+++ python/web_scraper.py
@@ -1,14 +1,6 @@
-import requests, sys 
-from bs4 import BeautifulSoup as bs # or just "import bs4"
-
-if len(sys.argv) != 2:
-    print "Argument Error"
-    sys.exit()
-
-# some of my own vars
+import requests 
 
 def scrape(link,save=False):
-#print sys.argv[1]
     try:
         res = requests.get(link)
         res.raise_for_status()
@@ -25,27 +17,5 @@ def scrape(link,save=False):
         for chunk in res.iter_content(100000):
             write2.write(chunk)
         write2.close()
-        print "Saved Scrapings on " + fn + " ...just for you ;-)"
-    return res.text # could just give out the obj???
-
-lt = scrape(sys.argv[1])
-parsed = bs(lt, "html.parser") # can also take file obj
-dd_menu = parsed.select('iframe[name="schedule"]')[0]
-print dd_menu
-
-menu = scrape(dd_menu['src'])
-parsed = bs(menu, "html.parser")
-form = parsed.select('form')[0]['action']
-#print form
-#print
-options = parsed.select('option')
-for opt in options:
-    r = opt['value']
-    print r
-
-sched = {}
-print
-p = options[1]['value']
-print p
-ps = {p[0]:p[1]}
-print requests.post("http://cruiser.colgate.edu/schedule-resultsDT.php", data={"routes":p}).text
+        print "Saved Scrapings on " + fn
+    return res 
